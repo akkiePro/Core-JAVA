ef4e13e4e991176164e9d1caa4c7b61ce511d29f
Akash Prajapati <akashprajapati1849@gmail.com>
2024-02-25 21:53:03 +0530

    String heap concept

---
+++ Telusko Java Course/008_String.java
@@ -0,0 +1,22 @@
+class StringDemo {
+    // every string stores inside String Constant Pool
+    public static void main(String[] args) {
+        String s = "Hello String";
+        String s1 = new String("Hello again String");
+        // s and s1 both are same first one is shorthand method to initialize String
+
+        String name = "Akash";
+        System.out.println(name);
+        // System.out.println(name.hashCode());
+
+        String member = "Akash";
+        System.out.println(member);
+
+        System.out.println(name == member);     // there's only one "Akash" which is referenced by member and name.
+
+        String name2 = "akash";
+        name2 = name2 + " prajapati";
+        System.out.println(name2);  // akash prajapati
+        // after watching 008_String_Heap_Updated.png you realise that String is immutable. because, it creates new heap storage location for updated string
+    }
+}
